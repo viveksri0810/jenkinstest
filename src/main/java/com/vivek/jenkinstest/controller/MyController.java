8b2b67877db085ba1e0f20ed8a30c8706c36f098
Vivek <viveksri0810@gmail.com>
2022-06-10 10:28:32 +0530

    adding junk controller

--- src/main/java/com/vivek/jenkinstest/controller/MyController.java
+++ src/main/java/com/vivek/jenkinstest/controller/MyController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class MyController {
 
@@ -12,6 +15,16 @@ public class MyController {
     @GetMapping("/getmessage")
     public String getMessage(){
         return "Hello!!..  welcome to my page";
+    }
+
+
+    @GetMapping("/junk")
+    public Map<String,String> junk(){
+        Map<String,String > junkMap=new HashMap<>();
+        junkMap.put("first name","vivek");
+        junkMap.put("last name", "sri");
+        junkMap.put("message","i am junk");
+        return junkMap;
     }
 
 }
